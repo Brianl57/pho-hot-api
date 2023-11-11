98768b3ae35746d8b3c7cd77a5d997e9b4318955
Brian Le <brianle@Brians-MacBook-Pro.local>
2023-11-10 23:55:47 -0800

    finished backend

---
+++ routes/menu-items.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { google } = require("googleapis");
+const mongoose = require("mongoose");
+const MenuItem = require("../models/menu_itemModel")
+
+const router = express.Router();
+
+const ids = [];
+
+// fecthing data from GoogleSheets and updating database 
+router.get('/', async (req, res) => {
+    const auth = new google.auth.GoogleAuth({
+        keyFile: process.env.KEY_FILE,
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+    })
+
+    // create client instance for auth 
+    const client = await auth.getClient();
+
+    // create instance of Google sheets api
+    const googlesheets =  google.sheets({ version: "v4", auth: client });
+    
+    const spreadsheetId = process.env.sheetID;
+
+    // Get metadeta about spreadsheet
+    const metaData = await googlesheets.spreadsheets.get({
+        auth,
+        spreadsheetId,
+    })
+
+    // read rows from spreadsheet
+    const getRows = await googlesheets.spreadsheets.values.get({
+        auth, 
+        spreadsheetId,
+        range: "Sheet1"
+    })
+
+    const menu_items = getRows.data.values;
+
+    res.send(menu_items);
+})
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const menuItems = require('./routes/menu-items');
+
+require('dotenv').config();
+
+// express app
+app = express();
+
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+ 
+
+app.use('/pho-hot', menuItems)
+
+
+//connecting to mongoDB
+// mongoose.connect(process.env.mongo_URI)
+//     .then(() => {
+//         console.log("Successully connected to MongoDB")
+//         app.listen(4000, () => {
+//             console.log(`listening on PORT ${process.env.PORT}`)
+//         })
+//     })
+//     .catch((error) => {
+//         console.log(error)
+//     })
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening on PORT ${process.env.PORT}` )
+})
